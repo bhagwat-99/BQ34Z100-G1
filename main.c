e3b467c03e294a49d03105c440471bf3a3a9573d
bhagwat-99 <shindebhagavat030@gmail.com>
2022-04-19 12:39:42 +0530

    main update current reading

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include "battery.h"
 
+const char *i2c_bus = "/dev/apalis-i2c1";
 
 int main()
 {
-        if(i2c_init()<0)
+        if(i2c_init(i2c_bus)<0)
         {
                 return -1;
         }
 
         printf("current : %f\n",current());
 
-        i2c_close();
+        i2c_close(i2c_bus);
         return 0;
 }
